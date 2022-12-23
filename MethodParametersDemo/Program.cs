bc46f73a34396165b31b1e68832799d9d4f59eb4
vikul rathod <vikul.rathod.net@gmail.com>
2022-12-23 09:43:47 +0530

    method and method parameters

---
+++ MethodParametersDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //string n = "100";
+        //int i;
+        //bool b = int.TryParse(n, out i);
+
+        //string name = "Main";
+        //Console.WriteLine(name); // Main
+        //PrintA(name);
+        //Console.WriteLine(name); // Main
+
+        //string name = "Main";
+        ////string name;
+        ///*        Console.WriteLine(name);*/ // Main
+        //PrintA(ref name);
+        //Console.WriteLine(name); // PrintA
+
+        int a = 10, b = 5, add, sub, mul, div;
+
+        Operation(a, b, out add, out sub, out mul, out div);
+        Console.WriteLine($"Addition : {add}");
+        Console.WriteLine($"Substraction : {sub}");
+        Console.WriteLine($"Multiplication : {mul}");
+        Console.WriteLine($"Division : {div}");
+
+        Console.ReadLine();
+    }
+
+    //static void PrintA(string name)
+    //{
+    //    name = "PrintA";
+    //}
+
+    //static void PrintA(ref string name)
+    //{
+    //    // name = "PrintA";
+    //}
+
+    static void Operation(int a, int b, 
+        out int add, out int sub, out int mul, out int div)
+    {
+        add = a + b;
+        sub = a - b;
+        mul = a * b;
+        div = a / b;
+    }
+}
---
+++ MethodsDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Main() Starts");
+
+//        PrintA();
+
+//        string r1 = PrintB();
+//        Console.WriteLine(r1);
+
+//        PrintC("Vishal");
+
+//        int r2 = PrintD("Vishal");
+//        Console.WriteLine(r2); // 6
+
+//        Console.WriteLine("Main() End");
+//        Console.ReadLine();
+//    }
+
+//    // method does not have return type and parameters
+//    static void PrintA()
+//    {
+//        Console.WriteLine("PrintA() Method");
+//    }
+
+//    // method has return type and no parameters
+
+//    static string PrintB()
+//    {
+//        Console.WriteLine("PrintB() Method");
+//        return "HELLO from PrintB()";
+//    }
+
+//    // method has no return type and has parameters
+//    static void PrintC(string name)
+//    {
+//        Console.WriteLine($"Hello, {name}");
+//    }
+
+//    // method has return type and has parameters
+//    static int PrintD(string name)
+//    {
+//        return name.Length;
+//    }
+//}
+
+class Program
+{
+    // Main() method = Calling Method
+    static void Main()
+    {
+        string r1 = PrintA("Vishal"); // Vishal = method argument
+        Console.WriteLine(r1);
+
+        string n1 = "Mahesh";
+        r1 = PrintA(n1); // n1 = method argument
+        Console.WriteLine(r1);
+
+        // PrintB(false);
+
+        PrintB(true);
+
+        Console.ReadLine();
+    }
+
+    // PrintA() method = Called Method
+    static string PrintA(string name) // name - method parameter
+    {
+        return "GOOD MORNING, " + name;
+    }
+
+    static void PrintB(bool b)
+    {
+        if (!b)
+        {
+            return;
+        }
+        Console.WriteLine("Execute code from PrintB() Method");
+    }
+}
