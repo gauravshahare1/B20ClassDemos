5d29199374fad0dbcafee5db711ad06b3e6457ee
vikul rathod <vikul.rathod.net@gmail.com>
2023-01-06 09:49:43 +0530

    object type, var & dynamic keyword

---
+++ ObjectTypeDemo/Program.cs
@@ -0,0 +1,135 @@
+﻿
+using VHaaShNamespace;
+class Program
+{
+    static void Main()
+    {
+        #region ToString(), GetType()
+        //bool b1 = true;
+        //Console.WriteLine(b1.ToString());
+        //Type bt1 = b1.GetType();
+        //Console.WriteLine(bt1.FullName);
+        //Console.WriteLine(bt1.Name);
+
+        //int i1 = 10;
+        //Console.WriteLine(i1.ToString());
+        //Console.WriteLine(i1.GetType().Name);
+
+        //string s1 = "Vishal";
+        //Console.WriteLine(s1);
+
+        //Student s2 = new Student("Anil", "Rathod");
+        //Console.WriteLine(s2.ToString()); // Student
+
+        //Student s3 = new Student("Rahul", "Rathod");
+        //Console.WriteLine(s3.ToString()); // Student
+
+        //Console.WriteLine(s3.GetType().Name); // Student
+        //Console.WriteLine(s3.GetType().FullName); // VHaaShNamespace.Student
+
+        //Type st1 = typeof(Student);
+        //Console.WriteLine(st1.Name); // Student
+        //Console.WriteLine(st1.FullName); // VHaaShNamespace.Student
+
+        //s3.Print(typeof(Student));
+        //s3.Print(s3.GetType());
+        #endregion ToString(), GetType()
+
+        #region Equals()
+
+        //int i = 10;
+        //int j = 10;
+        //if(i.Equals(j))
+        //{
+        //    Console.WriteLine("i & j are EQUAL");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("i & j are NOT EQUAL");
+        //}
+
+        //string x = "Vishal";
+        //string y = "Vishal";
+        //if (x.Equals(y))
+        //{
+        //    Console.WriteLine("x & y are EQUAL");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("x & y are NOT EQUAL");
+        //}
+
+        //Student s1 = new Student("Anil", "Rathod");
+        //// Student s2 = s1;
+        //Student s2 = new Student("Anil", "Rathod");
+        //if (s1.Equals(s2))
+        //{
+        //    Console.WriteLine("s1 & s2 are EQUAL");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("s1 & s2 are NOT EQUAL");
+        //}
+
+        #endregion Equals()
+
+        int i = 10;
+        Console.WriteLine(i.GetHashCode());
+
+        string s = "Vishal";
+        Console.WriteLine(s.GetHashCode());
+
+        string s1 = "Mahesh";
+        Console.WriteLine(s1.GetHashCode());
+
+        Student o1 = new Student("Anil", "Rathod");
+        Console.WriteLine(o1.GetHashCode());
+        Student o2 = new Student("Anil", "Pawar");
+        Console.WriteLine(o2.GetHashCode());
+
+        int a = 10, b = 10;
+        bool b1 = a == b;
+        b1 = a.Equals(b);
+        b1 = a.GetHashCode() == b.GetHashCode();
+
+        Console.ReadLine();
+    }
+}
+
+namespace VHaaShNamespace
+{
+    class Student
+    {
+        string FirstName;
+        string LastName;
+
+        public Student(string fn, string ln)
+        {
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public void Print(Type type)
+        {
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Student s = (Student)obj;
+            return this.FirstName.Equals(s.FirstName) &&
+                this.LastName.Equals(s.LastName);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.FirstName.GetHashCode() ^ 
+                this.LastName.GetHashCode();
+        }
+    }
+}
---
+++ vardynamicKyewordDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.Intrinsics;
+
+class Program
+{
+    static void Main()
+    {
+        //int v1 = 10;
+        //v1 = "Vishal";
+        //char v2 = '$';
+        //double v3 = 15.2;
+        //Student v4 = new Student();
+        //v4.Print();
+
+        //object v1 = 10;
+        //v1 = "Vishal";
+        //object v2 = '$';
+        //object v3 = 15.2;
+        //object v4 = new Student();
+        //v4.Print();
+
+        //var v1 = new { FirstName = "Anil", LastName = "Rathod" };
+        //Console.WriteLine(v1.FirstName);
+        //Console.WriteLine(v1.LastName);
+
+
+        //var v1 = 10; // after this line v1 will be of type int
+        //// v1 = "Vishal";
+        //var v2 = '$'; // after this line v2 will be of type char
+        //var v3 = 15.2; // after this line v3 will be of type double
+        //var v4 = new Student(); // after this line v4 will be of type Student
+        //v4.Print();
+
+        var i = 100;
+
+        // var j = null;
+
+        //dynamic v1 = 10;
+        //v1 = "Vishal";
+        //dynamic v2 = '$';
+        //dynamic v3 = 15.2;
+        dynamic v4 = new Student();
+        v4.Print();
+        v4.MethodNahiye();
+
+        //dynamic s1 = new Student();
+        //dynamic s2 = new Student();
+        //Student s3 = s1 + s2;
+
+        //dynamic i = 10;
+        //Console.WriteLine(i); // 10
+        //i = "Vishal";
+        //Console.WriteLine(i); // Vishal
+
+        Console.ReadLine();
+    }
+}
+
+class Student
+{
+    // var Name;
+
+    public void Print()
+    {
+        Console.WriteLine("Student Class Print Method");
+    }
+}
