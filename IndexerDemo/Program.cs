6444f223b25d5a61b79d64f421818dc6ba4f5b12
vikul rathod <vikul.rathod.net@gmail.com>
2023-01-10 09:46:40 +0530

    property indexer demo

---
+++ IndexerDemo/Program.cs
@@ -0,0 +1,96 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //string[] names = new string[] { "Vishal", "Mahesh", "Dipak" };
+        //string n = names[1];
+        //Console.WriteLine(n); // Mahesh
+
+        //names[1] = "Shital";
+        //n = names[1];
+        //Console.WriteLine(n); // Shital
+
+        Student[] students = new Student[]
+        {
+        new Student(){ RollNumber = 101, Name = "Vishal", City = "Pune"},
+        new Student(){ RollNumber = 102, Name = "Mahesh", City = "Mumbai"},
+        new Student(){ RollNumber = 103, Name = "Dipak", City = "Pune"},
+        new Student(){ RollNumber = 104, Name = "Shital", City = "Pune"},
+        new Student(){ RollNumber = 105, Name = "Snehal", City = "Mumbai"}
+        };
+
+        Institute i1 = new Institute(students);
+
+        string name = i1[105]; // reading value
+        Console.WriteLine(name); // Snehal
+
+        i1[105] = "Priyanka"; // setting value
+        name = i1[105]; // reading value
+        Console.WriteLine(name); // Priyanka
+
+        Student s = i1["Vishal"];
+        Console.WriteLine($"{s.RollNumber} : {s.Name} : {s.City}");
+
+        Console.ReadLine();
+    }
+}
+
+class Student
+{
+    public int RollNumber { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+}
+
+class Institute
+{
+    public string InstituteName { get; set; }
+    private Student[] _students;
+
+    public Institute(Student[] students)
+    {
+        _students = students;
+    }
+
+    public string this[int rn]
+    {
+        get
+        {
+            for (int i = 0; i < _students.Length; i++)
+            {
+                if (_students[i].RollNumber == rn)
+                {
+                    return _students[i].Name;
+                }
+            }
+            return $"Student Not Found by Roll Number {rn}";
+        }
+
+        set
+        {
+            for (int i = 0; i < _students.Length; i++)
+            {
+                if (_students[i].RollNumber == rn)
+                {
+                    _students[i].Name = value;
+                }
+            }
+        }
+    }
+
+    public Student this[string name]
+    {
+        get
+        {
+            for (int i = 0; i < _students.Length; i++)
+            {
+                if (_students[i].Name == name)
+                {
+                    return _students[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ PropertyDemo/Program.cs
@@ -0,0 +1,142 @@
+﻿class Program
+{
+    static void Main()
+    {
+        #region Code 1
+
+        //Student s1 = new Student();
+        //// setting values / assigning values/ write
+        //s1.RollNumber = 1;
+        //s1.Name = "Vishal";
+        //s1.Passmark = 40;
+
+        //// getting values / reading values
+        //Console.WriteLine($"Roll Number : {s1.RollNumber}" +
+        //    $" Name : {s1.Name} Pass Mark : {s1.Passmark}");
+
+        #endregion Code 1
+
+        #region Code 2
+
+        //Student s1 = new Student();
+        //// s1.RollNumber = -1; // rollnumber cannot be negative
+        //// s1.SetRollNumber(-1);
+        //s1.SetRollNumber(1);
+        //// s1.Name = null; // if name is null then while display it should be disaply as No Name
+        //// s1.SetName(null);
+        //s1.SetName("Mahesh");
+        //// s1.Passmark = 22; // we should not be able to change its value
+
+        //Console.WriteLine($"Roll Number : {s1.GetRollNumber()}" +
+        //    $" Name : {s1.GetName()} Pass Mark : {s1.GetPassmark()}");
+
+        #endregion Code 2
+
+        Student s1 = new Student();
+        // setting values / assigning values/ write
+        s1.RollNumber = -1;
+        s1.Name = null;
+        //  s1.Passmark = 40;
+        s1.City = "Pune";
+
+        // getting values / reading values
+        Console.WriteLine($"Roll Number : {s1.RollNumber}" +
+            $" Name : {s1.Name} Pass Mark : {s1.Passmark}" +
+            $" City : {s1.City}");
+
+        Console.ReadLine();
+    }
+}
+
+//class Student
+//{
+//    private int _rollNumber;
+//    private string _name;
+//    private int _passmark = 40;
+
+//    public int GetRollNumber()
+//    {
+//        return _rollNumber;
+//    }
+
+//    public void SetRollNumber(int rn)
+//    {
+//        if (rn > 0)
+//        {
+//            _rollNumber = rn;
+//        }
+//        else
+//        {
+//            Console.WriteLine("RollNumber cannot be nagative");
+//        }
+//    }
+
+//    public void SetName(string name)
+//    {
+//        _name = name;
+//    }
+//    public string GetName()
+//    {
+//        return string.IsNullOrEmpty(_name) ? "No Name" : _name;
+//    }
+
+//    public int GetPassmark()
+//    {
+//        return _passmark;
+//    }
+//}
+
+class Student
+{
+    private int _rollNumber;
+    private string _name;
+    private int _passmark = 40;
+
+    public int RollNumber
+    {
+        get
+        {
+            return _rollNumber;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _rollNumber = value;
+            }
+            else
+            {
+                Console.WriteLine("RollNumber cannot be nagative");
+            }
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return string.IsNullOrEmpty(_name) ? "No Name" : _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+
+    public int Passmark
+    {
+        get
+        {
+            return _passmark;
+        }
+    }
+
+    private string _city;
+    public string City
+    {
+        get { return _city; }
+        set { _city = value; }
+    }
+
+    public string Email { get; set; }
+}
